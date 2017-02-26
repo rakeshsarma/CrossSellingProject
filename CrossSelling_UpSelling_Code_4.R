b39dc008661e45804e1a9688aec9c9e0dd5a27c7
Raki <raki@Rakeshs-MacBook-Pro.local>
2017-02-27 00:49:42 +0530

    Generated a new set of features and built an lm model

--- CrossSelling_UpSelling_Code_4.R
+++ CrossSelling_UpSelling_Code_4.R
@@ -11,27 +11,37 @@ library(caret)
 library(MASS) # for StepAIC calculation
 library(DMwR) # FOr calculating Error Metrics
 library(dplyr) # Make sure that Dplyr is loaded in last, as we need select funciton in dplyr
+library(infotheo)
+library(e1071)
 marketing_data<- read_excel("~/Data Science/CrossSellingProject/Target Marketing and cross selling - Data.xls")
 summary(marketing_data)
 str(marketing_data)
-unique(marketing_data$Job_Code)
+View(marketing_data)
 #replace space with underscore
 names(marketing_data) <- gsub(" ", "_", names(marketing_data))
 names(marketing_data) 
+unique(marketing_data$Job_Code)
+########################Preliminary investigation
+#boxplots
+#boxplot1: Branch_Id vs revenue
+marketing_data$Branch_ID<-as.factor(as.character(marketing_data$Branch_ID))
+boxplot(marketing_data$Ticket_Revenue~marketing_data$Branch_ID)
 
+#boxplot2: Customer_Type vs revenue
+marketing_data$Customer_Type<-as.factor(as.character(marketing_data$Customer_Type))
+boxplot(marketing_data$Ticket_Revenue~marketing_data$Customer_Type)
 # ######################### data preprocessing starts ########################
 # Step 0.1 : converting customer type into factor with 1,2,3
-marketing_data$Customer_Type_1<- as.factor(as.character(substring(text = marketing_data$Customer_Type, first = nchar(marketing_data$Customer_Type)-1, last = nchar(marketing_data$Customer_Type))))
+#marketing_data$Customer_Type_1<- as.factor(as.character(substring(text = marketing_data$Customer_Type, first = nchar(marketing_data$Customer_Type)-1, last = nchar(marketing_data$Customer_Type))))
 
 
 # ######################### data preprocessing ends########################
-#rolling the data to customer level
 #Step 1 : Order the data based on Customer ID and Call Duration
 marketing_data_1 <- marketing_data[order(marketing_data$Customer_ID,marketing_data$Call_Date),]
 #Sanity Check
 View(marketing_data_1)
 #Step 2: Remove unnecessay attributes, Use dplyr package to do this 
-marketing_data_2 <- select(marketing_data_1, -c(Customer_Name:Contact, Ticket_Number,Current_Email, Week_Ending_Date, Call_Time, Schedule_Time, Dispatch_Time, Complete_Time))
+marketing_data_2 <- select(marketing_data_1, -c(Customer_Name:Contact, Ticket_Number,Current_Email, Week_Ending_Date, Schedule_Time, Dispatch_Time, Complete_Time))
 #Sanity Check
 View(marketing_data_2)
 #Step 3: Take the difference of call dates
@@ -39,7 +49,7 @@ marketing_data_3<-marketing_data_2 %>% arrange(Customer_ID, Call_Date) %>% group
 View(marketing_data_3)
 #Step 4: Lets count the total number of rows for each customer
 Customer_count<-marketing_data_2 %>% group_by(Customer_ID) %>% summarise(count =  n()) %>% arrange(count)
-Customer_count_1<-marketing_data_4 %>% group_by(Customer_ID) %>% summarise(count =  n()) %>% arrange(count)
+#Customer_count_1<-marketing_data_4 %>% group_by(Customer_ID) %>% summarise(count =  n()) %>% arrange(count)
 View(Customer_count)
 #Step 5: Extracting the first and the last columns
 
@@ -49,6 +59,7 @@ marketing_data_4<- marketing_data_3 %>% arrange(Customer_ID, Call_Date) %>%
 # Sanity Check
 nrow(marketing_data_4) # 2116
 length(unique(marketing_data_4$Customer_ID)) #1058 customer ids
+
 ######################### Preparing Test data set Part 1/2 ##########
 #Step 5.1 Extracting the last columns for test data
 View(marketing_data_4)
@@ -86,7 +97,7 @@ marketing_data_test_2 $diff_call_complete <- as.Date(marketing_data_test_2$Compl
 #F2:- Train_data - Difference between Last_Service_date and Setup_date: Customer_age
 marketing_data_5$cust_age <- as.Date(marketing_data_5$Last_Service_Date)-as.Date(marketing_data_5$Setup_Date)
 View(marketing_data_5)
-#F2:- Train_data - Difference between Last_Service_date and Setup_date: Customer_age
+#F2:- Test_data - Difference between Last_Service_date and Setup_date: Customer_age
 marketing_data_test_2$cust_age <- as.Date(marketing_data_test_2$Last_Service_Date)-as.Date(marketing_data_test_2$Setup_Date)
 View(marketing_data_test_2)
 
@@ -99,11 +110,24 @@ marketing_data_test_2$month <- as.integer(substring(text = marketing_data_test_2
 View(marketing_data_test_2)
 
 #---------------------------------
+#F4 Train_Data - Have a newfeature to when a call was made, earlyMorning, Morning, evening, Night
+call_time_category<-discretize(marketing_data_5$Call_Time, nbins = 4, disc = "equalwidth")
+call_time_category <- apply(call_time_category, 2, as.factor)
+marketing_data_5$call_time_category <- call_time_category
+marketing_data_5$call_time_category <- as.factor(marketing_data_5$call_time_category)
+#F5 Test_Data - Have a newfeature to when a call was made, earlyMorning, Morning, evening, Night
+call_time_category_test<-discretize(marketing_data_test_2$Call_Time, nbins = 4, disc = "equalwidth")
+call_time_category_test <- apply(call_time_category_test, 2, as.factor)
+marketing_data_test_2$call_time_category <- call_time_category_test
+marketing_data_test_2$call_time_category <- as.factor(marketing_data_test_2$call_time_category)
+View(marketing_data_test_2)
+
+#---------------------------------
 # Step 8: Remove unnecessary features; Customer_id, Branch_ID, Bill_To,setupdate, 
 #last_service_date, call_date, complete date, Year_month, Schedule date
 marketing_data_6<- select(marketing_data_5, -ends_with(c("Date"))) 
 View(marketing_data_6)
-marketing_data_7 <- select(marketing_data_6, -c(Branch_ID:Bill_To, Year_Month) )
+marketing_data_7 <- select(marketing_data_6, -c(Branch_ID:Bill_To, Year_Month, Call_Time) )
 rm(marketing_data_6)
 View(marketing_data_7)
 #Reoreding the variables with target variable in the front
@@ -116,7 +140,7 @@ marketing_data_7$Customer_Type<- as.factor(as.character(substring(
 
 #Step 8.1: Doing step 8 for for test data
 marketing_data_test_3<- select(marketing_data_test_2, -ends_with(c("Date"))) 
-marketing_data_test_4 <- select(marketing_data_test_3, -c(Branch_ID:Bill_To, Year_Month) )
+marketing_data_test_4 <- select(marketing_data_test_3, -c(Branch_ID:Bill_To, Year_Month,Call_Time) )
 rm(marketing_data_test_3)
 View(marketing_data_test_4)
 #Reoreding the variables with target variable in the front
@@ -138,6 +162,7 @@ marketing_data_7$diff_call_complete <- as.numeric(marketing_data_7$diff_call_com
 marketing_data_7$cust_age <- as.numeric(marketing_data_7$cust_age)
 marketing_data_7$diff <- as.numeric(as.character(marketing_data_7$diff))
 marketing_data_7$month <- as.factor(as.character(marketing_data_7$month))
+marketing_data_7$call_time_category <- as.factor(as.numeric(marketing_data_7$call_time_category))
 #sanity check
 str(marketing_data_7)
 class(marketing_data_7)
@@ -155,7 +180,7 @@ marketing_data_test_4$diff_call_complete <- as.numeric(marketing_data_test_4$dif
 marketing_data_test_4$cust_age <- as.numeric(marketing_data_test_4$cust_age)
 marketing_data_test_4$diff <- as.numeric(as.character(marketing_data_test_4$diff))
 marketing_data_test_4$month <- as.factor(as.character(marketing_data_test_4$month))
-
+marketing_data_test_4$call_time_category<-as.factor(as.character(marketing_data_test_4$call_time_category))
 #sanity check
 str(marketing_data_test_4)
 class(marketing_data_test_4)
@@ -176,6 +201,13 @@ regr.eval(marketing_data_7$diff, predictions_on_train)
 # M2 : Best model using StepAIC
 model2 <- stepAIC(linear_model, direction = "both")
 summary(model2$anova)
+#M3 : svm model on train data
+formula<-as.formula(paste("diff~", paste(setdiff(colnames(marketing_data_7), "diff"), collapse = '+')))
+x_variables<- setdiff(colnames(marketing_data_7), "diff")
+svm_model<-svm(x =marketing_data_7[,x_variables], y = marketing_data_7$diff,data=marketing_data_7, 
+    type = "eps-regression", kernel = "linear", cost=10, gamma = 0.1 )
+
+
 
 #Step 11.1 
 ########################## Testing on test data #################
@@ -185,3 +217,4 @@ View(marketing_data_test_2)
 
 test_data_with_preds <- data.frame(marketing_data_test_4, predictions_on_test)
 View(test_data_with_preds)
+sum(abs(test_data_with_preds$diff- test_data_with_preds$predictions_on_test))/sum(test_data_with_preds$diff)
